66be6a7505df893a6e3fe6d1b012597b0fda7735
Víctor Guzmán <fawques@gmail.com>
2012-12-09 23:05:34 +0100

    Reiniciada aplicacion

--- index-test.php
+++ index-test.php
@@ -5,7 +5,7 @@
  */
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii-1.1.12.b600af/framework/yii.php';
+$yii=dirname(__FILE__).'/../../yii-1.1.12.b600af/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 
 // remove the following line when in production mode
