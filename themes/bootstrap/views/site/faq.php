714b218677e430430ca737d5ea5a4a967e4907a5
Pablo1990 <pablovm1990@gmail.com>
2013-02-10 17:37:28 +0100

    faq sin probar

---
+++ themes/bootstrap/views/site/faq.php
@@ -0,0 +1,19 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>F.A.Q.</h1>
+
+<div id="ctQueEs">
+	<h2>¿Qu&eacute es esto?</h2>
+</div>
+
+<div id="ctWhoAre">
+	<h2>¿Qui&eacuten est&acute detr&acute de esto?</h2>
+</div>
+
+<div id="ctOpen">
+	<h2>OpenSource... ¡Venga! ¿y qu&eacute m&acutes?</h2>
+</div>
