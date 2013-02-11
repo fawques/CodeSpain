d8b663bd596d5486ee3ccbdc27ff5cbc98a72ade
Pablo1990 <pablovm1990@gmail.com>
2013-02-11 21:52:35 +0100

    faq realizado

--- themes/bootstrap/views/site/faq.php
+++ themes/bootstrap/views/site/faq.php
@@ -4,16 +4,19 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>F.A.Q.</h1>
-
+<h1>Frequent Answer-Questions</h1>
+<br>
 <div id="ctQueEs">
 	<h2>¿Qu&eacute es esto?</h2>
+	<p>Esto es una web. Pero si quieres más información, es una web dedicada a eventos para desarrolladores. Es decir, queremos que existe en este emplazamiento un super-mega-calendario en el cual estén todos los eventos realizados en España para developers.</p>
 </div>
-
+<br>
 <div id="ctWhoAre">
-	<h2>¿Qui&eacuten est&acute detr&acute de esto?</h2>
+	<h2>¿Qui&eacuten est&aacute detr&aacute de esto?</h2>
+	<p>Somos unos universitarios, interesados en asistir a estos eventos, y que, al no encontrar una página que reuna a todos (o la mayoría de ellos), nos hemos visto casi obligados, a realizar este proyecto.</p>
 </div>
-
+<br>
 <div id="ctOpen">
-	<h2>OpenSource... ¡Venga! ¿y qu&eacute m&acutes?</h2>
+	<h2>OpenSource... ¡Venga! ¿y qu&eacute m&aacutes?</h2>
+	<p>Pues sí, así es. Está web es Open Source, de hecho, si quieres o simplemente tienes curiosidad por ver cómo se ha realizado y cómo está hecha la misma, puedes ser ir: o bien, a nuestro <a href="codespain.es/blog">blog</a> o, por otro lado, puedes ir a nuestro <a href="https://github.com/fawques/CodeSpain">repositorio</a>.</p>
 </div>
