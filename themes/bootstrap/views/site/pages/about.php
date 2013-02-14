2ba9a3ab32cc1e408940c37a940120435a925195
Pablo1990 <pablovm1990@gmail.com>
2013-02-14 20:40:19 +0100

    about terminado

---
+++ themes/bootstrap/views/site/pages/about.php
@@ -0,0 +1,37 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/about.css" />
+</head>
+
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - About';
+?>
+<h1>La people in the project</h1>
+<br>
+<br>
+<br>
+<div>
+	<img src="../../images/escolanix-1.jpg" class="imgAbout">
+	<span class="spanTitle">Escolanix</span>
+	<span class="spanText">Adri&aacuten Escolano D&iacuteaz, es un estudiante de Ingeniería Informática de la UA, apasionado de la tecnología,el deporte y el frikismo en general. Es un friki en toda regla, y hasta su sistema operativo principal es friki... Usa Arch Linux, ¡ya me direis! Por lo demás es un poco sosete, pero si le das cerveza o un ordenador ya mola un poco más. ¡Ah! Se me olvidaba, él se ha encargado de la BD, con esto creo que queda todo dicho.</span>
+</div>
+<br>
+<br>
+<br>
+<div>
+	<img src="../../images/victorius-1.jpg" class="imgAbout">
+	<span class="spanTitle">V&iacutectorius</span>
+	<span class="spanText">V&iacutector Guzm&aacuten Pedrajas se define tal que así, cito: "Uno estudia informática en la Universidad de Alicante. A uno le encantan las cosas con botones. Y uno se alegra de resultar útil." Como vereis muy normal no tiene que ser, pero bueno, es majete. Es el cerebro del grupo, él intenta poner paz entre Pablotao y Escolanix, pero normalmente acabamos en enzarzamientos que arreglamos en "la arena". Como dato, suele tener respuesta para casi cualquier cosa, con lo que dudas a él.</span>
+</div>
+<br>
+<br>
+<br>
+<div>
+	<img src="../../images/pablotao-1.jpg" class="imgAbout">
+	<span class="spanTitle">Pablotao</span>
+	<span class="spanText">Pablo Vicente Munuera. Se ha encargado de las frikadas en general y de lo irrelevante del proyecto. Por supuesto, también es estudiante en ingeniería Informática en la Universidad de Alicante. Se define así mismo como "Critico, transparente y honesto", con lo que probablemente sea todo lo contrario. Por lo demás, es un poco lerdete, pero normalmente cae bien.</span>
+</div>
+<br>
+<br>
+<h2>... Y juntos crearon CodeSpain.</h2>
