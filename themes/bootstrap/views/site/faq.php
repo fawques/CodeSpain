0a3b3f25afd3aae3db7c8135c6a36193547e72eb
Pablo1990 <pablovm1990@gmail.com>
2013-02-15 16:46:56 +0100

    retoques en about

--- themes/bootstrap/views/site/faq.php
+++ themes/bootstrap/views/site/faq.php
@@ -8,21 +8,22 @@ $this->pageTitle=Yii::app()->name;
 <br>
 <div id="ctQueEs">
 	<h2>¿Qu&eacute es esto?</h2>
-	<p>Esto es una web. Pero si quieres más información, es una web dedicada a eventos para desarrolladores. Es decir, queremos que existe en este emplazamiento un super-mega-calendario en el cual estén todos los eventos realizados en España para developers.</p>
+	<p>Esto es una web. Pero si quieres m&aacutes informaci&oacuten, es una web dedicada a eventos para desarrolladores. Es decir, queremos que existe en este emplazamiento un super-mega-calendario en el cual est&eacuten todos los eventos realizados en España para developers.</p>
 </div>
 <br>
 <div id="ctWhoAre">
 	<h2>¿Qui&eacuten est&aacute detr&aacute de esto?</h2>
-	<p>Somos unos universitarios, interesados en asistir a estos eventos, y que, al no encontrar una página que reuna a todos (o la mayoría de ellos), nos hemos visto casi obligados, a realizar este proyecto.</p>
-	<p>Para m&aacutes go to the <a href="site/page/about">About</a></p>
+	<p>Somos unos universitarios, interesados en asistir a estos eventos, y que, al no encontrar una p&aacutegina que reuna a todos (o la mayor&iacutea de ellos), nos hemos visto casi obligados, a realizar este proyecto.</p>
+	<p>Para m&aacutes go to the <a href="../site/page?view=about">About</a></p>
 </div>
 <br>
 <br>
 <div id="ctOpen">
 	<h2>OpenSource... ¡Venga! ¿y qu&eacute m&aacutes?</h2>
-	<p>Pues sí, así es. Está web es Open Source, de hecho, si quieres o simplemente tienes curiosidad por ver cómo se ha realizado y cómo está hecha la misma, puedes ser ir: o bien, a nuestro <a href="codespain.es/blog">blog</a> o, por otro lado, puedes ir a nuestro <a href="https://github.com/fawques/CodeSpain">repositorio</a>.</p>
+	<p>Pues s&iacute, as&iacute es. Est&aacute web es Open Source, de hecho, si quieres o, simplemente tienes curiosidad, por ver c&oacutemo se ha realizado y c&oacutemo est&aacute hecha la misma, puedes ser ir: o bien, a nuestro <a href="codespain.es/blog">blog</a> o, por otro lado, puedes ir a nuestro <a href="https://github.com/fawques/CodeSpain">repositorio</a>.</p>
 </div>
 <br>
 <div>
 	<h2>Tipos de eventos</h2>
 </div>
+<br>
