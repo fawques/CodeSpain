f26e4597f39986c4caaf2ef570dd12e6f83b38c2
Ignacio <ignacio@saldum.com>
2013-04-02 01:02:40 +0200

    Eliminado el atributo align
    El atributo align es deprecated, se debe emplear css para dicho fin. Y
    en este caso Bootstrap tiene la clase "pagination-centered" que es un
    text-align:center. Nunca habría que usar el atributo "style" en un
    html, para eso está el fichero .css
    
    Sustituidos los style de index.php con text: align-center por la clase
    de bootstrap correspondiente.
    
    Sustituidos los símbolos "¡" y "¿" por sus respectivos entity codes. De
    paso las "ñ" y otras vocales con tildes.

--- protected/views/detalleEvento/Index.php
+++ protected/views/detalleEvento/Index.php
@@ -5,27 +5,31 @@
 <div class="container-fluid">
 	<div class="row-fluid">
 		<div class="span10">
-			
+
 			<div class="span7">
 				<?php echo "<img src='$ModeloEvento->Imagen'/>" ?>
 			</div>
 
 			<div class="span5">
 				<h4> Datos evento </h4>
-				<b>Nombre del Evento:</b> <p><?php echo $ModeloEvento->Nombre ?></p> 
-				<b>Localización:</b> <p><?php echo $ModeloEvento->Lugar ?></p>
-				<b>Fecha de inicio:</b> <p><?php echo date("d-m-Y",strtotime($ModeloEvento->FechaIni)); ?></p> 
-				<b>Fecha de fin:</b> <p><?php echo date("d-m-Y",strtotime($ModeloEvento->FechaFin)); ?></p> 
-			</div>				
+				<b>Nombre del Evento:</b>
+				<p><?php echo $ModeloEvento->Nombre ?></p>
+				<b>Localizaci&oacute;n:</b>
+				<p><?php echo $ModeloEvento->Lugar ?></p>
+				<b>Fecha de inicio:</b>
+				<p><?php echo date("d-m-Y",strtotime($ModeloEvento->FechaIni)); ?></p>
+				<b>Fecha de fin:</b>
+				<p><?php echo date("d-m-Y",strtotime($ModeloEvento->FechaFin)); ?></p>
+			</div>
 		</div>
 
 		<div class="span10">
-				<h4> Descripción del evento </h4>
+				<h4> Descripci&oacute;n del evento </h4>
 				<?php echo "<p>".$ModeloEvento->Descripcion."</p>" ?>
 		</div>
 
 		<div class="span10">
-				
+
 			<?php  $this->widget('bootstrap.widgets.TbButton', array(
 			    'label'=>'Ver en mapa',
 			    'type'=>'primary', // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
