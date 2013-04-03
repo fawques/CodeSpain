759963ec8801be93baa58019b83a2cd120097417
Pablo1990 <pablovm1990@gmail.com>
2013-04-03 19:21:21 +0200

    añadido el botón de nuevo Evento

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -20,7 +20,7 @@
         array(
             'class'=>'bootstrap.widgets.TbMenu',
             'items'=>array(
-                array('label'=>'Home', 'url'=>array('/site/index')),
+                array('label'=>'Home', 'url'=>array('../')),
                 array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
                 array('label'=>'Contact', 'url'=>array('/site/contact')),
                 array('label'=>'F.A.Q.', 'url'=>array('/site/page', 'view'=>'faq')
@@ -31,8 +31,10 @@
                     array('label'=>'¿OpenSource?', 'url'=>array('/site/page', 'view'=>'faq', 'id'=>'ctOpen'))
                     ,)*/
                 ,),
+                array('label'=>'Nuevo Evento', 'url'=>array('/evento/create'), 'visible'=>!Yii::app()->user->isGuest),
                 array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
                 array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+                
 
             ),
 
