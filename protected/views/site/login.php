6f68c85955c2d923c54d796daf29b723a566b2a0
Pablo1990 <pablovm1990@gmail.com>
2013-04-07 13:41:28 +0200

    Login accesible

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -3,7 +3,7 @@
 <p>Accede con cualquiera de estos servicios</p>
 
 
-<a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Google" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/googleIcon.png' ?>"></a>
-<a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Twitter" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/TwitterIcon.jpg' ?>"></a>
-<a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Facebook" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/facebookIcon.jpg' ?>"></a>
+<a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Google" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/googleIcon.png' ?>" alt="Logo Google" value="Google"></a>
+<a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Twitter" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/TwitterIcon.jpg' ?>" alt="Logo Twitter" value="Twitter"></a>
+<a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Facebook" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/facebookIcon.jpg' ?>" alt="Logo Facebook" value="Facebook"></a>
 
