8c3d0846ea1c95da9947d2d10b71435cb194efde
Adrian Escolano <bl4ckf4lk0n@gmail.com>
2013-04-09 17:54:16 +0200

    Facebook eliminado momentaneamente. Again

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -5,5 +5,5 @@
 
 <a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Google" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/googleIcon.png' ?>" alt="Logo Google" value="Google"></a>
 <a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Twitter" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/TwitterIcon.jpg' ?>" alt="Logo Twitter" value="Twitter"></a>
-<a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Facebook" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/facebookIcon.jpg' ?>" alt="Logo Facebook" value="Facebook"></a>
+<!--<a href="<?php echo Yii::app()->request->baseUrl."/index.php/acceso/Facebook" ?>"><img width='50px' height='50px' src="<?php echo Yii::app()->request->baseUrl.'/images/facebookIcon.jpg' ?>" alt="Logo Facebook" value="Facebook"></a>-->
 
